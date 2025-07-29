70fb2ed6fd8881a701c34500f0226b4b9fb17483
sozcelyk43 <sozcelyk@gmail.com>
2025-07-30 00:10:31 +0300

    Add files via upload

---
+++ words_tr_2letters.js
@@ -0,0 +1,3 @@
+const wordListTr_2letters = [
+'ad', 'ak', 'al', 'an', 'ar', 'as', 'at', 'av', 'ay', 'az', 'bu', 'ek', 'el', 'en', 'er', 'es', 'et', 'ev', 'ez', 'eş', 'he', 'iç', 'il', 'in', 'ip', 'is', 'iş', 'üç', 'ne', 'ok', 'ol', 'on', 'ot', 'oy', 'öc', 'öz', 'su', 'şu'
+];
